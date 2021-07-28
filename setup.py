6cc4ce47d495cba1f1b717a0b03bcae636646174
Antonio <junior.phys@gmail.com>
2021-07-27 21:54:57 -0300

    fix version number

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 from os import path
 
 this_dir = path.abspath(path.dirname(__file__))
-with open(path.join(this_dir, "README.md")) as f:
+with open(path.join(this_dir, "README.rst")) as f:
     long_description = f.read()
 
-__version__ = "0.0.3"
+__version__ = "0.0.1"
 for line in open(path.join("OpenSMOG", "__init__.py")):
     if line.startswith("__version__"):
         exec(line.strip())
@@ -14,26 +14,22 @@ setup(
     name="OpenSMOG",
     version=__version__,
     description="Structure-based Models for Biomolecules using OpenMM",
-    url="",
+    url="https://github.com/junioreif/OpenSMOG",
     author="Antonio Bento de Oliveira Junior,Vinicius de Godoi Contessoto",
     author_email="antonio.oliveira@rice.edu,contessoto@rice.edu",
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.4',
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         'Programming Language :: Python :: 3.7',
         "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Natural Language :: English",
     ],
+    include_package_data=True,
     packages=find_packages(),
-    install_requires=['numpy'],
+    install_requires=['numpy', 'lxml'],
     entry_points={"console_scripts": ["CLINAME=OpenSMOG._cli:main"]},
     zip_safe=True,
     long_description=long_description,
-    long_description_content_type="text/x-rst",
 )
