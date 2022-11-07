a6334fb21f6890ce83c28da586888315dafe54f0
Vinícius Contessoto <contessoto@rice.edu>
2022-11-07 17:10:40 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,8 +28,8 @@ copyright = '2020-2021 The Center for Theoretical Biological Physics (CTBP) - Ri
 author = 'Antonio B. Oliveira Jr., Vinícius G. Contessoto & Paul Whitford'
 
 # The full version, including alpha/beta/rc tags
-version = '1.1.0'
-release = '1.1.0'
+version = '1.1.1'
+release = '1.1.1'
 
 # -- General configuration ---------------------------------------------------
 
