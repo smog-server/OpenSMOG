1fd4a41b429971cb29d278e2950d688138d128a0
Paul <p.whitford@neu.edu>
2024-12-09 14:58:09 -0500

    1.2 label added

--- OpenSMOG/__init__.py
+++ OpenSMOG/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.2rc1'
+__version__ = '1.2'
 # if you change __version__, remember to change SBM.version
 from .OpenSMOG_Reporter import forcesReporter, stateReporter
 from .OpenSMOG import SBM
