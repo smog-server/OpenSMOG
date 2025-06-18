bbf29d3374ec8615e6f0db3408fbc58217c7dbda
Matheus Mello <mfmello@outlook.com>
2025-06-18 08:38:41 -0500

    fixed unused parameters in `runAA`

--- OpenSMOG/OpenSMOG.py
+++ OpenSMOG/OpenSMOG.py
@@ -141,7 +141,7 @@ be more appropriate.
                 self.platform = Platform.getPlatformByName(tryplat)
                 break
 
-    def runAA(name='smogtest',time_step=0.002, nsteps=10000,collision_rate=1.0, r_cutoff=0.65, temperature=0.5,gro="smog.gro",top="smog.top",xml="smog.xml",saveinterval=1000,trajectoryName=None, trajectoryFormat='dcd', energies=True, energiesName=None, energy_components=False, energy_componentsName=None, logFileName='OpenSMOG.log'):
+    def runAA(name='smogtest', time_step=0.002, nsteps=10000,collision_rate=1.0, r_cutoff=0.65, temperature=0.5, gro="smog.gro", top="smog.top", xml="smog.xml", saveinterval=1000, trajectoryName=None, trajectoryFormat='dcd', energies=True, energiesName=None, energy_components=True, energy_componentsName=None, logFileName='OpenSMOG.log'):
         R"""A quick way to start a simulation with default parameters that are typical with the standard all-atom SMOG model.
     You can also override many parameters, if needed. This may not be suitable for production runs.
 
@@ -191,7 +191,7 @@ be more appropriate.
             SMOGrun.loadSystem(Grofile=gro, Topfile=top, Xmlfile=xml, noxml=False)
         SMOGrun.createSimulation()
         SMOGrun.minimize(tolerance=1)
-        SMOGrun.createReporters(trajectory=True, energies=True, energy_components=True, interval=saveinterval,trajectoryName=trajectoryName, trajectoryFormat=trajectoryFormat, energiesName=energiesName, energy_componentsName=energy_componentsName, logFileName='OpenSMOG.log')
+        SMOGrun.createReporters(trajectory=True, energies=energies, energy_components=energy_components, interval=saveinterval,trajectoryName=trajectoryName, trajectoryFormat=trajectoryFormat, energiesName=energiesName, energy_componentsName=energy_componentsName, logFileName=logFileName)
         SMOGrun.run(nsteps=nsteps, report=True, interval=saveinterval)
 
 
